e43d6f6f86c013345a58888b76865ba0f3c12a2f
Mika <mika@wappia.se>
2021-03-28 14:28:33 +0200

    Missed changes

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,4 @@
 #!/usr/bin/env node
 
-import { addExtension, run } from "clubhouse-gears"
-
-function kaboom(date, warningMessage) {
-  robotArm.initiateSelfDestruct(date)
-  speakers.tts(warningMessage)
-}
-
-addExtension(`kaboom`, kaboom)
+import { run } from "clubhouse-gears"
 run()
-
-const customers = [`BigAppleInc`, `DarknessInc`] // or fetch this from some external API
-const projects = await api.listProjects()
-api.createStory({
-  name: "Send invoices",
-  description: `Send invoices to ${customers.join(`, `)}`,
-  projectId: projects.find((it) => it.name === `Abstrakt`).id,
-  deadline: new Date().getTime() + 60000 * 60 * 24 * 7,
-})
